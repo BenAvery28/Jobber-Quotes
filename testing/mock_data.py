f957d8cda1cab978d008fdf5e1a0671c09b4a410
Andrei Lazic <alazic@outlook.com>
2025-08-27 02:15:28 -0500

    Commented and tested mock data on the program using pytest functions.

--- testing/mock_data.py
+++ testing/mock_data.py
@@ -1,9 +1,19 @@
+#testing/mock_data.py
 import random
 from datetime import datetime, timedelta
 
 def generate_mock_quote(quote_id=None):
+    """
+    Generate a mock Jobber quote (something like this will be input when an order is filled)
+    params:
+     - quote_id (str, optional): if none, generates a random quote id
+    Returns:
+     - dictionary quote containing: id, client (email + city), amount (total price)
+    """
+
     if quote_id is None:
         quote_id = f"Q{random.randint(100, 999)}"
+
     return {
         "id": quote_id,
         "client": {
@@ -13,7 +23,19 @@ def generate_mock_quote(quote_id=None):
         "amounts": {"totalPrice": round(random.uniform(100, 5000), 2)},
     }
 
+
 def generate_mock_visits(count=5):
+    """
+    Generate a list of fake job visits starting from today's date
+    (simulates situation that will occur when actually deployed)
+    params:
+    - count (int): number of fake visits to generate
+    returns:
+     list[dict]: Each visit has:
+            - startAt: ISO start datetime
+            - endAt: ISO end datetime (2 hours after start)
+    """
+
     return [
         {
             "startAt": (datetime.now() + timedelta(days=i)).isoformat(),
